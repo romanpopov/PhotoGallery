f16a27cbae34f6d9ca1928dbc290ed7bf699a536
Roman Popov <popovro@gmail.com>
2017-03-16 23:31:22 +0300

    implement startup

--- src/PhotoGallery/Startup.cs
+++ src/PhotoGallery/Startup.cs
@@ -7,31 +7,142 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using PhotoGallery.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Serialization;
+using PhotoGallery.Infrastructure.Repositories;
+using PhotoGallery.Infrastructure.Services.Abstact;
+using PhotoGallery.Infrastructure.Services;
+using System.Security.Claims;
+using System.IO;
+using Microsoft.Extensions.FileProviders;
 
 namespace PhotoGallery
 {
     public class Startup
     {
+        private static string _applicationPath = string.Empty;
+        private static string _contentRootPath = string.Empty;
+        public Startup(IHostingEnvironment env)
+        {
+            _applicationPath = env.WebRootPath;
+            _contentRootPath = env.ContentRootPath;
+            // Setup configuration sources.
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(_contentRootPath)
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsDevelopment())
+            {
+                // This reads the configuration keys from the secret store.
+                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets();
+            }
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+        public IConfigurationRoot Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<PhotoGalleryContext>(options =>
+                options.UseSqlServer(Configuration["Data:PhotoGalleryConnection:ConnectionString"]));
+
+            // Repositories
+            services.AddScoped<IPhotoRepository, PhotoRepository>();
+            services.AddScoped<IAlbumRepository, AlbumRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserRoleRepository, UserRoleRepository>();
+            services.AddScoped<IRoleRepository, RoleRepository>();
+            services.AddScoped<ILoggingRepository, LoggingRepository>();
+
+            // Services
+            services.AddScoped<IMembershipService, MembershipService>();
+            services.AddScoped<IEncryptionService, EncryptionService>();
+
+            services.AddAuthentication();
+
+            // Polices
+            services.AddAuthorization(options =>
+            {
+                // inline policies
+                options.AddPolicy("AdminOnly", policy =>
+                {
+                    policy.RequireClaim(ClaimTypes.Role, "Admin");
+                });
+
+            });
+
+            // Add MVC services to the services container.
+            services.AddMvc()
+            .AddJsonOptions(opt =>
+            {
+                var resolver = opt.SerializerSettings.ContractResolver;
+                if (resolver != null)
+                {
+                    var res = resolver as DefaultContractResolver;
+                    res.NamingStrategy = null;
+                }
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            //loggerFactory.AddConsole();
+            // this will serve up wwwroot
+            app.UseFileServer();
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            // this will serve up node_modules
+            var provider = new PhysicalFileProvider(
+                Path.Combine(_contentRootPath, "node_modules")
+            );
+            var _fileServerOptions = new FileServerOptions();
+            _fileServerOptions.RequestPath = "/node_modules";
+            _fileServerOptions.StaticFileOptions.FileProvider = provider;
+            _fileServerOptions.EnableDirectoryBrowsing = true;
+            app.UseFileServer(_fileServerOptions);
 
-            app.Run(async (context) =>
+            AutoMapperConfiguration.Configure();
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
-                await context.Response.WriteAsync("Hello World!");
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true
             });
+
+            // Custom authentication middleware
+            //app.UseMiddleware<AuthMiddleware>();
+
+            // Add MVC to the request pipeline.
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                // Uncomment the following line to add a route for porting Web API 2 controllers.
+                //routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
+            });
+
+            DbInitializer.Initialize(app.ApplicationServices, _applicationPath);
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+              .UseKestrel()
+              .UseContentRoot(Directory.GetCurrentDirectory())
+              .UseIISIntegration()
+              .UseStartup<Startup>()
+              .Build();
+
+            host.Run();
         }
     }
 }
